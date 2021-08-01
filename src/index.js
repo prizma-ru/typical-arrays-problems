396e99a0d34dcfe5ff46fba6902de629c70c79fa
prizma-ru <dmitry.glotov@gmail.com>
2021-08-02 00:20:10 +0300

    init: start task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = []) {
+  let min = Math.min(...array);
+  if (min != Infinity) {
+      return min
+  }
+  else {
+      return 0
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = []) {
+  let max = Math.max(...array);
+  if (max != Infinity) {
+      return max
+  }
+  else {
+      return 0
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = []) {       
+  if (array != Infinity) {
+      return array.reduce((a, b) => a + b)/array.length;
+  }
+  else {
+      return 0
+  }    
 }
+
+
